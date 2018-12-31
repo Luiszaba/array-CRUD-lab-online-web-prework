14eaac635156fc7fd1058ec535aec7bab124e319
Luis Zabala <luis.zabala076@gmail.com>
2018-12-31 22:13:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,8 +15,7 @@ def add_element_to_start_of_array(array, element)
   array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array, element)
-  array = ["cat"]
+def remove_element_from_end_of_array
   array.pop(element)
 end
 
