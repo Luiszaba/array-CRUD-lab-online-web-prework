a8303fc6693102d6fe0d590854383ad33a87c037
Luis Zabala <luis.zabala076@gmail.com>
2018-12-31 22:06:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,23 @@
 def create_an_empty_array
   array  = []
 end
+
+def create_an_array
+  array = [1,2,3,4]
+end
+
+def add_element_to_end_of_array
+  array.push
+end
+
+def add_element_to_start_of_array
+  array.unshift
+end
+
+def remove_element_from_end_of_array
+  array.pop
+end
+
+def remove_element_from_end_of_array
+  array.shift
+end
